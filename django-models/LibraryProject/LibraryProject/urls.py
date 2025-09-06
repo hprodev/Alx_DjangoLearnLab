17d82a8209414fa946661631c8efb7d61af6fd94
Abdulhakeem Ibrahim <icreate.hakeem@gmail.com>
2025-09-06 09:56:03 +0100

    Task 1

--- django-models/LibraryProject/LibraryProject/urls.py
+++ django-models/LibraryProject/LibraryProject/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('bookshelf.urls')),
+    path('', include('relationship_app.urls')),
 ]
 
 if settings.DEBUG:
---
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -0,0 +1,23 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # Task 1: Views and URL Configuration
+    path('books/', views.list_books, name='list_books'),
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+    
+    # Task 2: User Authentication URLs
+    path('login/', views.CustomLoginView.as_view(), name='login'),
+    path('logout/', views.CustomLogoutView.as_view(), name='logout'),
+    path('register/', views.register, name='register'),
+    
+    # Task 3: Role-based Access Control URLs
+    path('admin/', views.admin_view, name='admin_view'),
+    path('librarian/', views.librarian_view, name='librarian_view'),
+    path('member/', views.member_view, name='member_view'),
+    
+    # Task 4: Custom Permissions URLs
+    path('add_book/', views.add_book, name='add_book'),
+    path('edit_book/<int:book_id>/', views.edit_book, name='edit_book'),
+    path('delete_book/<int:book_id>/', views.delete_book, name='delete_book'),
+]
