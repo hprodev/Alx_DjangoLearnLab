281ac5af946107c94d866b1038f74ff4232841ae
Abdulhakeem Ibrahim <icreate.hakeem@gmail.com>
2025-09-19 07:29:37 +0100

    Task 0

--- api_project/api/models.py
+++ api_project/api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    publication_date = models.DateField(null=True, blank=True)
+    isbn = models.CharField(max_length=13, unique=True)
+    pages = models.IntegerField(null=True, blank=True)
+    
+    class Meta:
+        ordering = ['title']
+    
+    def __str__(self):
+        return f"{self.title} by {self.author}"
