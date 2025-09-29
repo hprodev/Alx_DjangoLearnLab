65010a11875471cc3bce3b277859613f97ab2694
Abdulhakeem Ibrahim <icreate.hakeem@gmail.com>
2025-09-29 10:54:46 +0100

    Task 2

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -4,6 +4,7 @@ from rest_framework import filters
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Book
 from .serializers import BookSerializer
+from django_filters import rest_framework
 
 class BookListView(generics.ListAPIView):
     """
